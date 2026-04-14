186c63137382f37fc9401d0b8d0c15f35af261c2
Rulax <104562580+rulax113@users.noreply.github.com>
2026-04-14 22:57:27 +0200

    feat: add character detail view

--- src/app/features/character-detail/character-detail.ts
+++ src/app/features/character-detail/character-detail.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Character, STATUS_LABELS } from '../../core/models/character.model';
+import { MOCK_CHARACTERS } from '../../core/models/characters.data';
 
 @Component({
   selector: 'app-character-detail',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './character-detail.html',
   styleUrl: './character-detail.scss',
 })
-export class CharacterDetail {}
+export class CharacterDetail implements OnInit {
+  character: Character | undefined;
+  statusLabels = STATUS_LABELS;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.character = MOCK_CHARACTERS.find(c => c.id === id);
+  }
+}
