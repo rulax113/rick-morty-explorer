8748207642f2148d30f00db4e9219fe30b125858
Rulax <104562580+rulax113@users.noreply.github.com>
2026-04-14 21:05:32 +0200

    feat: configure routing with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home').then(m => m.Home)
+  },
+  {
+    path: 'characters',
+    loadComponent: () => import('./features/characters/characters').then(m => m.Characters)
+  },
+  {
+    path: 'characters/:id',
+    loadComponent: () => import('./features/character-detail/character-detail').then(m => m.CharacterDetail)
+  },
+  { path: '**', redirectTo: 'home' }
+];
