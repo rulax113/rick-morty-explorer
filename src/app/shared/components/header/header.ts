ba191e732234de297603330fd816a6ab74fdc94f
Rulax <104562580+rulax113@users.noreply.github.com>
2026-04-14 21:15:16 +0200

    feat: implement header component with navigation and routing

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  title = 'Marvel Library';
+}
